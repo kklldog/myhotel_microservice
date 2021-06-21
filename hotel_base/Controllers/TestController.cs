fc11d974ebe6cbe2e3acc3727668c8274c0f734a
mjzhou <kklldog@qq.com>
2021-06-21 17:37:48 +0800

    update

---
+++ hotel_base/Controllers/TestController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace hotel_base.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+        private readonly ILogger _logger;
+
+        public TestController(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<TestController>();
+        }
+
+        [HttpGet("TestLogSeq")]
+        public string TestLogSeq()
+        {
+            _logger.LogTrace("this is a test log for trace level .");
+            _logger.LogDebug("this is a test log for debug level .");
+            _logger.LogInformation("this is a test log for info level .");
+            _logger.LogWarning("this is a test log for warning level .");
+            _logger.LogError(new Exception("this is a ex for seq log ."), "this is a test log for error level .");
+
+            return "ok";
+        }
+    }
+}
--- hotel_base/Program.cs
+++ hotel_base/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using NLog.Web;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -14,11 +15,24 @@ namespace hotel_base
     {
         public static void Main(string[] args)
         {
-            var builder = new ConfigurationBuilder()
-            .SetBasePath(Directory.GetCurrentDirectory())
-            .AddJsonFile("appsettings.json")
-            .Build() ;
-            CreateHostBuilder(args).Build().Run();
+
+            var logger = NLog.Web.NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
+            try
+            {
+                logger.Debug("init main");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception exception)
+            {
+                //NLog: catch setup errors
+                logger.Error(exception, "Stopped program because of exception");
+                throw;
+            }
+            finally
+            {
+                // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
+                NLog.LogManager.Shutdown();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -30,6 +44,7 @@ namespace hotel_base
                         options.ListenAnyIP(6003);
                     });
                     webBuilder.UseStartup<Startup>();
-                });
+                })
+                .UseNLog();
     }
 }
